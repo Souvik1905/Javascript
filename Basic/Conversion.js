03aaeaee1d8c1466358f69d2e9aa0b3adf86cdf1
Souvik Daa <dasrohan1905@gmail.com>
2025-08-27 17:48:32 +0000

    Conversion Notes

---
+++ Basic/Conversion.js
@@ -0,0 +1,42 @@
+
+//let score =true
+
+/*onsole.log(typeof score);
+console.log(typeof (score))
+
+//let changeNumber = Number (score);
+console.log(typeof changeNumber);
+console.log(changeNumber)
+//true=>1 && false=>0*/
+
+let Id = undefined
+
+//console.log(Id)
+ 
+let changeId =Number(Id)
+//console.log(typeof changeId)
+//console.log(changeId)
+
+
+let logIN =null
+
+//console.log (logIN)
+
+let valueInLog=Number(logIN)
+//console.log(typeof valueInLog)
+//console.log(valueInLog)
+
+
+// null converted to 0 when used Number
+
+// "33"=> 33
+//"22abc"=> NaN
+//"" => false. "Souvik"=>true
+ 
+let name ="hii"
+
+console.log(typeof name)
+
+let changeName = Boolean(name)
+console.log(typeof changeName)
+console.log(changeName)
